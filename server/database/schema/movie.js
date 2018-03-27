cd1617d91b4cdbe9c55313ad557c510fa56999c5
DESKTOP-BE6T737\xiawa <xiawang1024@icloud.com>
2018-03-27 15:09:06 +0800

    feat:增加数据库movieSchema

---
+++ server/database/schema/movie.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const Mixed = Schema.Types.Mixed
+
+const movieSchema = new Schema({
+    doubanId:String,
+    rate:Number,
+    title:String,
+    summary:String,
+    video:String,
+    poster:String,
+    cover:String,
+
+    videoKey:String,
+    posterKey:String,
+    coverKey:String,
+
+    rawTitle:String,
+    movieTypes:[String],
+    pubdate:Mixed,
+    year:Number,
+
+    tags:Array,
+
+    meta:{
+        createdAt:{
+            type:Date,
+            default:Date.now()
+        },
+        updatedAt:{
+            type:Date,
+            default:Date.now()
+        }
+    }
+})
+
+mongoose.model('Movie', movieSchema)
+

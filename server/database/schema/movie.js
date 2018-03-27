95826bb1308d9b49b281017be7671e54fb8be809
DESKTOP-BE6T737\xiawa <xiawang1024@icloud.com>
2018-03-27 16:43:51 +0800

    增加user.js

--- server/database/schema/movie.js
+++ server/database/schema/movie.js
@@ -3,7 +3,10 @@ const Schema = mongoose.Schema
 const Mixed = Schema.Types.Mixed
 
 const movieSchema = new Schema({
-    doubanId:String,
+    doubanId:{
+        type:String,
+        unique:true
+    },
     rate:Number,
     title:String,
     summary:String,
@@ -32,6 +35,15 @@ const movieSchema = new Schema({
             default:Date.now()
         }
     }
+})
+
+movieSchema.pre('save', next => {
+    if(this.isNew) {
+        this.meta.createdAt = this.meta.updatedAt = Date.now()
+    }else{
+        this.meta.updatedAt = Date.now()
+    }
+    next()
 })
 
 mongoose.model('Movie', movieSchema)
